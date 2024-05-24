93110a9030a1f0bf4a6708d7d402a069c8a4c81e
packetchaos <itprofguru@gmail.com>
2024-05-24 11:17:52 -0700

    1.1.2 - Added error correction to scan details

--- ivan/plugins/scan.py
+++ ivan/plugins/scan.py
@@ -43,28 +43,82 @@ def start(scan_id, targets):
 @click.argument('scan_id')
 def details(scan_id):
     sc = tenb_connection()
-    scan_details = sc.scan_instances.details(id=scan_id)
+    try:
+        scan_details = sc.scan_instances.details(id=scan_id)
+    except:
+        click.echo("\nCheck your permissions to the scan or the scan ID; An Error occured\n")
+        exit()
 
     click.echo("\nScan Details")
     click.echo("-" * 75)
     click.echo()
 
-    # Grab scan data
-    scan_duration = scan_details['scanDuration']
-    scanned_ips = scan_details['scannedIPs']
-    start_time = arrow.get(float(scan_details['startTime']))
-    current_status = scan_details['status']
-    total_checks = scan_details['totalChecks']
-    total_ips = scan_details['totalIPs']
-    completed_checks = scan_details['progress']['completedChecks']
-    completed_ips = scan_details['progress']['completedIPs']
-    checks_per_host = scan_details['progress']['checksPerHost']
-    scanned_size = scan_details['progress']['scannedSize']
-    deadhost_size = scan_details['progress']['deadHostSize']
-    distributed_size = scan_details['progress']['distributedSize']
-    average = (int(scan_duration) / int(scanned_size)) / 60
+    try:
+        scan_duration = scan_details['scanDuration']
+    except:
+        scan_duration = "none"
+
+    try:
+        scanned_ips = scan_details['scannedIPs']
+    except:
+        scanned_ips = "none"
+
+    try:
+        start_time = arrow.get(float(scan_details['startTime']))
+    except:
+        start_time = "none"
+
+    try:
+        current_status = scan_details['status']
+    except:
+        current_status = "none"
+
+    try:
+        total_checks = scan_details['totalChecks']
+    except:
+        total_checks = "none"
+
+    try:
+        total_ips = scan_details['totalIPs']
+    except:
+        total_ips = "none"
+
+    try:
+        completed_checks = scan_details['progress']['completedChecks']
+    except:
+        completed_checks = "none"
+
+    try:
+        completed_ips = scan_details['progress']['completedIPs']
+    except:
+        completed_ips = "none"
+    try:
+        checks_per_host = scan_details['progress']['checksPerHost']
+    except:
+        checks_per_host = "none"
+    try:
+        scanned_size = scan_details['progress']['scannedSize']
+    except:
+        scanned_size = "none"
+    try:
+        deadhost_size = scan_details['progress']['deadHostSize']
+    except:
+        deadhost_size = "none"
+    try:
+        distributed_size = scan_details['progress']['distributedSize']
+    except:
+        distributed_size = "none"
+    try:
+        average = (int(scan_duration) / int(scanned_size)) / 60
+    except:
+        average = "none"
+    try:
+        delta = int(total_ips) - int(scanned_ips)
+    except:
+        delta = "none"
 
     click.echo("{:40s}{}".format("Current Status: ", current_status))
+    click.echo("{:40s}{}".format("Number of IPs not scanned", delta))
     click.echo("{:40s}{}".format("Scanned Start time: ", start_time.format("MM-DD-YYYY HH:mm:ss")))
     click.echo("{:40s}{}{}".format("Scan Duration: ", scan_duration, " Seconds"))
 
@@ -102,52 +156,64 @@ def details(scan_id):
     scanner_details = scan_details['progress']['scanners']
 
     for scanners in scanner_details:
-        chunk_completed = scanners['chunkCompleted']
-        completed_checks = scanners['completedChecks']
-        dead_host_size = scanners['deadHostSize']
-        distributed_size_by_scanner = scanners['distributedSize']
-        scanner_name = scanners['name']
-        asset_count = scanners['scannedSize']
+        try:
+            chunk_completed = scanners['chunkCompleted']
+            completed_checks = scanners['completedChecks']
+            dead_host_size = scanners['deadHostSize']
+            distributed_size_by_scanner = scanners['distributedSize']
+            scanner_name = scanners['name']
+            asset_count = scanners['scannedSize']
 
-        click.echo("{:40s}{}".format("Scanner Name", scanner_name))
-        click.echo("-" * 50)
-        click.echo("{:40s}{}".format("   -Total Chunks Completed", chunk_completed))
-        click.echo("{:40s}{}".format("   -Total Checks Completed", completed_checks))
-        click.echo("{:40s}{}".format("   -Dead hosts found by the Scanner", dead_host_size))
-        click.echo("{:40s}{}".format("   -IPs distributed to the scanner", distributed_size_by_scanner))
-        click.echo("{:40s}{}".format("   -Assets found by the scanner", asset_count))
-        click.echo("-" * 50)
-        click.echo()
+            click.echo("{:40s}{}".format("Scanner Name", scanner_name))
+            click.echo("-" * 50)
+            click.echo("{:40s}{}".format("   -Total Chunks Completed", chunk_completed))
+            click.echo("{:40s}{}".format("   -Total Checks Completed", completed_checks))
+            click.echo("{:40s}{}".format("   -Dead hosts found by the Scanner", dead_host_size))
+            click.echo("{:40s}{}".format("   -IPs distributed to the scanner", distributed_size_by_scanner))
+            click.echo("{:40s}{}".format("   -Assets found by the scanner", asset_count))
+            click.echo("-" * 50)
+            click.echo()
+        except TypeError:
+            click.echo("\nError Getting Scanner Data\n")
+            pass
+        except KeyError():
+            click.echo("\nError Getting Scanner Data\n")
+            pass
     click.echo("-" * 50)
     click.echo("-" * 50)
 
     click.echo("\nImport Details")
     click.echo("-" * 75)
     click.echo()
+    try:
+        import_duration = scan_details['importDuration']
+        import_finish = arrow.get(float(scan_details['importFinish']))
+        import_start = arrow.get(float(scan_details['importStart']))
+        import_status = scan_details['importStatus']
 
-    import_duration = scan_details['importDuration']
-    import_finish = arrow.get(float(scan_details['importFinish']))
-    import_start = arrow.get(float(scan_details['importStart']))
-    import_status = scan_details['importStatus']
-
-    click.echo("{:40s}{}{}".format("Import Duration", import_duration, " Seconds"))
-    click.echo("{:40s}{}".format("Import Start Time", import_start.format("MM-DD-YYYY HH:mm:ss")))
-    click.echo("{:40s}{}".format("Import Finish Time", import_finish.format("MM-DD-YYYY HH:mm:ss")))
-    click.echo("{:40s}{}".format("Import Status", import_status))
-    click.echo("-" * 50)
-    click.echo("-" * 50)
+        click.echo("{:40s}{}{}".format("Import Duration", import_duration, " Seconds"))
+        click.echo("{:40s}{}".format("Import Start Time", import_start.format("MM-DD-YYYY HH:mm:ss")))
+        click.echo("{:40s}{}".format("Import Finish Time", import_finish.format("MM-DD-YYYY HH:mm:ss")))
+        click.echo("{:40s}{}".format("Import Status", import_status))
+        click.echo("-" * 50)
+        click.echo("-" * 50)
+    except:
+        click.echo("\nError getting any import results\n")
 
     click.echo("\nError Details")
     click.echo("-" * 75)
     click.echo()
+    try:
+        error_details = scan_details['errorDetails']
+        error_sync_details = scan_details['ioSyncErrorDetails']
+        import_errors = scan_details['importErrorDetails']
 
-    error_details = scan_details['errorDetails']
-    error_sync_details = scan_details['ioSyncErrorDetails']
-    import_errors = scan_details['importErrorDetails']
+        click.echo("{:40s}{}".format("Error Details:\n", error_details))
+        click.echo("{:40s}{}".format("Tenable VM Sync Error Details:\n", error_sync_details))
+        click.echo("{:40s}{}".format("Import Error Details:\n", import_errors))
 
-    click.echo("{:40s}{}".format("Error Details:\n", error_details))
-    click.echo("{:40s}{}".format("Tenable VM Sync Error Details:\n", error_sync_details))
-    click.echo("{:40s}{}".format("Import Error Details:\n", import_errors))
+    except:
+        click.echo("\nIronically there was an Error getting any error results\n")
 
 
 @scan.command(help="Export a Scan")
